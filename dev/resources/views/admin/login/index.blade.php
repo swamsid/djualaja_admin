a0ff0197f4b2277c0b24cd2c6bfcb1308ea74d51
dirga ambara <swamsid@gmail.com>
2018-05-30 22:28:54 +0700

    a

--- dev/resources/views/admin/login/index.blade.php
+++ dev/resources/views/admin/login/index.blade.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>D'Jualaja | Login User</title>
+  <title>D'Jualaja | Login Admin</title>
 
    <!-- plugins:css -->
   <link rel="stylesheet" href="{{ url('dev/node_modules/mdi/css/materialdesignicons.min.css') }}" tppabs="http://www.bootstrapdash.com/demo/purple/node_modules/mdi/css/materialdesignicons.min.css">
