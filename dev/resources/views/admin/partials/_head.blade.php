805dda5ae7eb390374fce3e5ae4519138b24bafc
dirga <swamsid@gmail.com>
2018-08-27 18:41:38 +0700

    cek

--- dev/resources/views/admin/partials/_head.blade.php
+++ dev/resources/views/admin/partials/_head.blade.php
@@ -1,6 +1,7 @@
 <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
+  <meta name="viewport" content="width=1300">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>D'Jualaja | @yield("title")</title>
   
